1675bc71f98291273ab6233555297dc3933fbc76
Nikita Shilnikov <fg@flashgordon.ru>
2019-05-26 00:15:32 +0300

    Improve interrupt specs

--- spec/intergration/interrupt_spec.rb
+++ spec/intergration/interrupt_spec.rb
@@ -5,22 +5,28 @@ RSpec.describe 'handle interruption' do
   include Dry::Effects::Handler[state: :counter_b, as: :inner]
 
   it 'aborts execution with payload' do
+    reached = false
     result = outer(10) do
       catch_halt do
         inner(20) { halt :done }
+        reached = true
       end
     end
 
     expect(result).to eql([10, :done])
+    expect(reached).to be(false)
   end
 
   it 'aborts execution without payload' do
+    reached = false
     result = outer(10) do
       catch_halt do
         inner(20) { halt }
+        reached = true
       end
     end
 
     expect(result).to eql([10, nil])
+    expect(reached).to be(false)
   end
 end
