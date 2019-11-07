d2a5210352846112df43d6ae723bc3ea8a704ca7
Nikita Shilnikov <fg@flashgordon.ru>
2019-11-07 12:22:36 +0300

    Simplify coverage setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,10 @@
 
 require 'bundler/setup'
 
-if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
-  require 'yaml'
-  rubies = YAML.safe_load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
-  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
-
-  if RUBY_VERSION == latest_mri
-    require 'simplecov'
-    SimpleCov.start do
-      add_filter '/spec/'
-    end
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
   end
 end
 
