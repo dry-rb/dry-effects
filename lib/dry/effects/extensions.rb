4a6c79c543bff14ad8fc0518f2c2d67234f3f5fb
Nikita Shilnikov <fg@flashgordon.ru>
2019-11-24 22:19:59 +0300

    Add extension with RSpec patches to make some features work with dry-effects
    In particular, pending specs rely on Thread.current. I don't anticipate any problems with the patches since a) fortunately, RSpec doesn't use Thread.current a lot, b) patches are not invasive.

--- lib/dry/effects/extensions.rb
+++ lib/dry/effects/extensions.rb
@@ -11,3 +11,7 @@ end
 Dry::Effects.register_extension(:system) do
   require 'dry/effects/extensions/system'
 end
+
+Dry::Effects.register_extension(:rspec) do
+  require 'dry/effects/extensions/rspec'
+end
---
+++ lib/dry/effects/extensions/rspec.rb
@@ -0,0 +1,19 @@
+# These patches make those rspec parts depending on Thread.current
+# play nice with dry-effects.
+# They've been used in production for months before been added here.
+
+RSpec::Support.singleton_class.prepend Module.new {
+  include Dry::Effects.Reader(:rspec, as: :effect_local_data)
+
+  def thread_local_data
+    effect_local_data { super }
+  end
+}
+
+RSpec::Core::Runner.prepend Module.new {
+  include Dry::Effects::Handler.Reader(:rspec, as: :run_with_data)
+
+  def run_specs(*)
+    run_with_data(RSpec::Support.thread_local_data) { super }
+  end
+}
