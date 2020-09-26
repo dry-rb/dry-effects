546d19db8c3efdc6756e13847e898ff4df50c070
Nikita Shilnikov <fg@flashgordon.ru>
2020-09-27 00:26:06 +0300

    Use common rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'bundler/setup'
 
 require_relative 'support/coverage'
 require_relative 'support/warnings'
+require_relative "support/rspec_options"
 
 begin
   require 'pry-byebug'
@@ -26,11 +27,6 @@ Warning.ignore(/__FILE__/)
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.filter_run_when_matching :focus
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
