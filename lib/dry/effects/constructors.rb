b80482576f28c78510e6f86918d6d24d58b40045
Nikita Shilnikov <fg@flashgordon.ru>
2022-10-17 11:47:07 +0300

    Load missing effect constructors via method_missing

--- lib/dry/effects/constructors.rb
+++ lib/dry/effects/constructors.rb
@@ -3,9 +3,27 @@
 module Dry
   module Effects
     module Constructors
+      # @api private
       def self.register(name, &block)
         define_method(name, &block)
         module_function name
+      end
+
+      # @api private
+      def self.method_missing(name, *args, &block)
+        effect_name = name.to_s
+
+        if effect_name[0].match?(/[A-Z]/)
+          Effects.const_get(name)
+
+          if respond_to?(name)
+            public_send(name, *args, &block)
+          else
+            raise ArgumentError, "no constructor for #{name} effect"
+          end
+        else
+          super
+        end
       end
     end
   end
