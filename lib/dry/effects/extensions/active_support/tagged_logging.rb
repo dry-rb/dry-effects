5fa52b4b2c7f918045310db34590e788c429c66c
Nikita Shilnikov <fg@flashgordon.ru>
2024-03-06 14:34:56 +0100

    Fix specs by checking if AS properly isolates global state

--- lib/dry/effects/extensions/active_support/tagged_logging.rb
+++ lib/dry/effects/extensions/active_support/tagged_logging.rb
@@ -2,12 +2,18 @@
 
 require "active_support/tagged_logging"
 
-ActiveSupport::TaggedLogging::Formatter.prepend(Module.new {
-  def current_tags
-    thread_key = @thread_key ||= "activesupport_tagged_logging_tags:#{object_id}"
-    unless Thread.current.thread_variable_get(thread_key)
-      Thread.current.thread_variable_set(thread_key, [])
+begin
+  require "active_support/isolated_execution_state"
+
+  # it's not needed when state is already isolated
+rescue LoadError
+  ActiveSupport::TaggedLogging::Formatter.prepend(Module.new {
+    def current_tags
+      thread_key = @thread_key ||= "activesupport_tagged_logging_tags:#{object_id}"
+      unless Thread.current.thread_variable_get(thread_key)
+        Thread.current.thread_variable_set(thread_key, [])
+      end
+      Thread.current.thread_variable_get(thread_key)
     end
-    Thread.current.thread_variable_get(thread_key)
-  end
-})
+  })
+end
