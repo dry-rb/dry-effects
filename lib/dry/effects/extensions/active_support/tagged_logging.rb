849cd6faad7da16a33aeb78204802f104f10c9cf
Nikita Shilnikov <fg@flashgordon.ru>
2024-11-30 21:18:44 +0100

    Prefer __id__ over object_id

--- lib/dry/effects/extensions/active_support/tagged_logging.rb
+++ lib/dry/effects/extensions/active_support/tagged_logging.rb
@@ -9,7 +9,7 @@ begin
 rescue LoadError
   ActiveSupport::TaggedLogging::Formatter.prepend(Module.new {
     def current_tags
-      thread_key = @thread_key ||= "activesupport_tagged_logging_tags:#{object_id}"
+      thread_key = @thread_key ||= "activesupport_tagged_logging_tags:#{__id__}"
       unless Thread.current.thread_variable_get(thread_key)
         Thread.current.thread_variable_set(thread_key, [])
       end
